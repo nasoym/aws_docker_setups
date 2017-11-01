a71c18fd89c2b472310fca0669b39f131959b931
Sinan Goo <sinan.goo@hypoport.de>
2017-11-01 14:13:12 +0100

    update config and dashboard

--- haproxy_worker_setup_1/benchmark.sh
+++ haproxy_worker_setup_1/benchmark.sh
@@ -11,15 +11,9 @@ function ensure_service_available() {
 }
 
 : ${grafan_host:="http://grafana:3000"}
+: ${haproxy_host:="http://haproxy:80/"}
 
 apk --no-cache add apache2-utils curl jq
-
-# echo "initial wait"
-# sleep 10
-ensure_service_available "http://haproxy:80/"
-
-echo "run apache benchmark"
-ab -n 100 -c 10 http://haproxy:80/
 
 echo "ensure grafana is running"
 ensure_service_available "${grafan_host}"
@@ -45,12 +39,31 @@ for file in $(find /grafana/dashboards -type f) ; do
 done
 echo
 
+echo "ensure haproxy is responding"
+ensure_service_available "${haproxy_host}"
+
+epoche_start="$(date +%s)"
+
+echo "run apache benchmark"
+ab -s 300 -n 100000 -c 50 "${haproxy_host}"
+# ab -s 300 -n 1000 -k -c 5 "${haproxy_host}"
+
 echo "final wait"
 sleep 10
 
+epoche_end="$(date +%s)"
+second_duration="$(( $epoche_end - $epoche_start + 30 ))"
+
 echo "render dashboard"
 curl -s --user "admin:admin" \
-  "${grafan_host}/render/dashboard/db/dashboard?orgId=1&from=now-5m&to=now&kiosk&tz=CET&width=1200" \
+  "${grafan_host}/render/dashboard/db/dashboard\
+?orgId=1\
+&from=now-${second_duration}s\
+&to=now\
+&kiosk\
+&tz=CET\
+&width=1200\
+" \
   > /grafana_render/render_$(date +%FT%H_%M_%S).png
 
 echo "finished"
